87935edc78d6fe45f47169add5ee7acb5f2a6258
resqiar <resqiageng@gmail.com>
2021-04-14 21:48:50 +0700

    handle send message

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const handleMessage = require('../services/handleMessage')
+const handlePostback = require('../services/handlePostback')
+
 router.post('/webhook', (req, res) => {
     const requestBody = req.body
     /**
@@ -9,19 +12,28 @@ router.post('/webhook', (req, res) => {
      * @False => SEND 404
      */
     if (requestBody.object === 'page') {
-
         // Iterates over each entry - there may be multiple if batched
-        requestBody.entry.forEach(entry => {
-            /** 
-             * Gets the message. entry.messaging is an array, but 
+        requestBody.entry.forEach((entry) => {
+            /**
+             * Gets the message. entry.messaging is an array, but
              * will only ever contain one message, so we get index 0
-            */
-            const webhook_event = entry.messaging[0];
-            console.log("webhook_event", webhook_event);
+             */
+            const webhook_event = entry.messaging[0]
+            console.log('webhook_event', webhook_event)
 
             // SENDER ID
-            const sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
+            const sender_psid = webhook_event.sender.id
+            console.log('Sender PSID: ' + sender_psid)
+
+            /**
+             * Check if the event is a message or postback and
+             * pass the event to the appropriate handler function
+             */
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         })
         res.send('EVENT_RECEIVED')
     } else {
@@ -31,7 +43,7 @@ router.post('/webhook', (req, res) => {
 
 /**
  * Adds support for GET requests to our webhook
- * This code adds support for the Messenger Platform's webhook verification to webhook. 
+ * This code adds support for the Messenger Platform's webhook verification to webhook.
  * This is required to ensure webhook is authentic and working.
  */
 router.get('/webhook', (req, res) => {
@@ -40,14 +52,14 @@ router.get('/webhook', (req, res) => {
     // Query Params
     const mode = req.query['hub.mode']
     const token = req.query['hub.verify_token']
-    const challenge = req.query['hub.challenge'];
+    const challenge = req.query['hub.challenge']
 
     // Checks if token and mode exist
     if (mode && token) {
         if (token === VERIFY_TOKEN && mode === 'subscribe') {
             // Responds with the message from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
+            console.log('WEBHOOK_VERIFIED')
+            res.status(200).send(challenge)
         } else {
             // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403)
--- src/services/callSendApi.js
+++ src/services/callSendApi.js
@@ -1,4 +1,23 @@
 // Handles messaging_postbacks events
 module.exports = callSendApi = (sender_psid, response) => {
+    // message body
+    const request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
 
+    // request URL
+    const REQUEST_URL = `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.ACCESS_TOKEN}`
+
+    // Send the HTTP request to the Messenger Platform
+    fetch(REQUEST_URL, {
+        method: "POST",
+        body: request_body
+    }).then(() => {
+        console.log('message sent!')
+    }).catch(err => {
+        console.error("Unable to send message:" + err);
+    })
 }
