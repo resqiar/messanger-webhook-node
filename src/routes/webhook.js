81082355815ba7eee66ed3a8933b58a59fc3eca0
resqiar <resqiageng@gmail.com>
2021-04-14 12:17:51 +0700

    webhook routes

---
+++ src/routes/webhook.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/webhook', (req, res) => {
+})
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express().use(express.json())
 const PORT = process.env.PORT || 3031
+
+/**
+ * @Log every request to the console
+ */
+app.use(morgan('dev'))
+
+/**
+ * @Route path for webhook endpoint
+ */
+app.use(require('./routes/webhook'))
 
 /**
  * @Listen incoming connections on PORT
